056f106edb0732965e83e93a6f94b196ee4193fd
Dennis <dennis.hsieh@agoda.com>
2020-04-13 18:36:42 +0800

    MarginItemDecorator

---
+++ library/src/main/java/com/yundom/view/MarginItemDecorator.kt
@@ -0,0 +1,26 @@
+package com.yundom.view
+
+import android.graphics.Rect
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+class MarginItemDecorator(
+        private val verticalSpacing: Int,
+        private val horizontalSpacing: Int,
+        private val columnsCount: Int
+) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+            outRect: Rect,
+            view: View,
+            parent: RecyclerView,
+            state: RecyclerView.State
+    ) {
+        val position = parent.getChildAdapterPosition(view)
+        outRect.apply {
+            left = horizontalSpacing
+            top = if (position < columnsCount) verticalSpacing else 0
+            right = if ((position + 1) % columnsCount == 0) horizontalSpacing else 0
+            bottom = verticalSpacing
+        }
+    }
+}
